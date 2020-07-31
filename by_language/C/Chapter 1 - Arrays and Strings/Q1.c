755e1360cb79be9d3dee0f0c0a15aaada9e3407e
Stephen Smith <StephenASmith@protonmail.com>
2020-07-30 21:45:00 -0400

    first commit

---
+++ by_language/C/Chapter 1 - Arrays and Strings/Q1.c
@@ -0,0 +1,40 @@
+/*
+    Is Unique: Implement and algorithm to determine if a string has all unique characters.
+
+               What if you can't use additional data structures?
+*/
+
+//Assumption - Only Ascii Strings
+#include <string.h>
+#include <stdio.h>
+int isUnique(char* str);
+int main(){
+    char* string = "This is a Test String";
+    char* passstring = "qwertyu";
+    
+    if(isUnique(string)){
+        printf("\"%s\" is unique\n", string);
+    } else {
+        printf("\"%s\" is not unique\n", string);
+    }
+
+    if(isUnique(passstring)){
+        printf("\"%s\" is unique\n", passstring);
+    } else {
+        printf("\"%s\" is not unique\n", passstring);
+    }
+return 0;
+}
+
+int isUnique(char* str){
+int flagArray[126] = {0}; //assuming normal ascii
+int i;
+for(i = 0; i < strlen(str); i++){
+    flagArray[str[i]]++;
+    if(flagArray[str[i]] > 1){
+        return 0;
+    }
+}
+
+return 1;
+}
